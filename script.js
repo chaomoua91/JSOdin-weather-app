5081a9fe049cc9b8ccc6fc10d2036dd83ae4a477
Chao Moua <chaomoua91@gmail.com>
2024-05-30 19:47:09 -0500

    laidout what functions i expect to code in.

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 const apiKey = "ad80c83c62944a4cbc111505242905";
+
+weatherForm.addEventListener("submit", (event) => {});
+
+async function getWeatherData(city) {}
+
+function displayWeatherInfo(data) {}
+
+function getWeatherEmoji(weatherId) {}
+
+function displayError(message) {}
