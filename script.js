acdaa9bf17ae2c80c9fe9ae091dad274058d43a1
Chao Moua <chaomoua91@gmail.com>
2024-05-30 19:43:37 -0500

    added in variables to get classes form html and api key

---
+++ script.js
@@ -0,0 +1,4 @@
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "ad80c83c62944a4cbc111505242905";
