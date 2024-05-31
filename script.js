05027f05e21f930c17912a3d92dea7a279292308
Chao Moua <chaomoua91@gmail.com>
2024-05-30 19:54:55 -0500

    wrote code for displaying error message if you do not enter a city

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 const apiKey = "ad80c83c62944a4cbc111505242905";
 
-weatherForm.addEventListener("submit", (event) => {});
+weatherForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const city = cityInput.value;
+
+  if (city) {
+  } else {
+    displayError("Please enter a City");
+  }
+});
 
 async function getWeatherData(city) {}
 
@@ -11,4 +20,12 @@ function displayWeatherInfo(data) {}
 
 function getWeatherEmoji(weatherId) {}
 
-function displayError(message) {}
+function displayError(message) {
+  const errorDisplay = document.createElement("p");
+  errorDisplay.textContent = message;
+  errorDisplay.classList.add("errorDisplay");
+
+  card.textContent = "";
+  card.style.display = "flex";
+  card.appendChild(errorDisplay);
+}
