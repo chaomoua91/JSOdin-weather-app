be23d0d4a36e1e3c2ab231640df031bd1a76ac93
Chao Moua <chaomoua91@gmail.com>
2024-06-04 17:40:39 -0500

    finished with gif and getting the right weather to display

--- script.js
+++ script.js
@@ -23,18 +23,75 @@ weatherForm.addEventListener("submit", async (event) => {
 
 async function getWeatherData(city) {
   const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
-
   const response = await fetch(apiUrl);
 
   if (!response.ok) {
     throw new Error("Could not fetch weather data");
   }
+
   return await response.json();
 }
 
-function displayWeatherInfo(data) {}
+function displayWeatherInfo(data) {
+  console.log(data);
+  const {
+    location: { name },
+    current: {
+      temp_f,
+      humidity,
+      condition: { text },
+    },
+  } = data;
+  card.textContent = "";
+  card.style.display = "flex";
 
-function getWeatherEmoji(weatherId) {}
+  const cityDisplay = document.createElement("h1");
+  const tempDisplay = document.createElement("p");
+  const humidityDisplay = document.createElement("p");
+  const descDisplay = document.createElement("p");
+  const weatherEmoji = document.createElement("img");
+
+  cityDisplay.textContent = name;
+  tempDisplay.textContent = `${temp_f}°F`;
+  humidityDisplay.textContent = `Humidity: ${humidity}%`;
+  descDisplay.textContent = text;
+  weatherEmoji.textContent = getGiphy(text);
+
+  cityDisplay.classList.add("cityDisplay");
+  tempDisplay.classList.add("tempDisplay");
+  humidityDisplay.classList.add("humidityDisplay");
+  descDisplay.classList.add("descDisplay");
+  weatherEmoji.classList.add("weatherEmoji");
+
+  card.appendChild(cityDisplay);
+  card.appendChild(cityDisplay);
+  card.appendChild(tempDisplay);
+  card.appendChild(humidityDisplay);
+  card.appendChild(descDisplay);
+  card.appendChild(weatherEmoji);
+}
+
+async function getGiphy(text) {
+  try {
+    const img = document.querySelector("img");
+    let keyWord = text;
+    if (keyWord == "Clear") {
+      keyWord = "Clear Sky";
+    }
+
+    const response = await fetch(
+      "https://api.giphy.com/v1/gifs/translate?api_key=RVtCd0LhavcjcRbA3EQUbYWsRoEdLwSI&s=" +
+        keyWord,
+      { mode: "cors" }
+    );
+    const giphyResponse = await response.json();
+    console.log(giphyResponse);
+    img.style.display = "";
+    img.src = giphyResponse.data.images.original.url;
+  } catch (err) {
+    displayError("cannot find gif");
+  }
+}
 
 function displayError(message) {
   const errorDisplay = document.createElement("p");
